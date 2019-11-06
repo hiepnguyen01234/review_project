1eb74c11ec17c310936899d61c223c7a9d00227c
Thien <menkisede2@gmail.com>
2019-11-06 13:03:19 +0700

    session

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module ReviewProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-  end
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
+
+   end
 end
