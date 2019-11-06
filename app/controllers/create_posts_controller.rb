52daac9498bf91ebeefdd13c55c8acd3077600d9
hiep <viephan01234@gmail.com>
2019-11-06 19:19:31 +0700

    create post

---
+++ app/controllers/create_posts_controller.rb
@@ -0,0 +1,5 @@
+class CreatePostsController < ApplicationController
+	def home
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "/category", to: 'categories#home'
+  get "/create_post", to: 'create_posts#home'
   devise_for :users
 end
